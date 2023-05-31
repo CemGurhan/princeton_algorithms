4ca76890d9ac60a6d342d4c46ce2cd8c57267fd5
Cem Gurhan <cemg@live.co.uk>
2023-05-31 21:39:39 +0100

    add quick select

--- playground/QuickSelect/quickSelect.go
+++ playground/QuickSelect/quickSelect.go
@@ -1,3 +1,24 @@
 package quickselect
 
-func QuickSelect()
+func QuickSelect(array *[]int, start int, end int) {
+	if start < end {
+		pivotIndex := partition(array, start, end)
+		QuickSelect(array, start, pivotIndex-1)
+		QuickSelect(array, pivotIndex+1, end)
+	}
+}
+
+func partition(array *[]int, start int, end int) int {
+	pivot := (*array)[end]
+	positionalIndex := start
+
+	for i := start; i <= end-1; i++ {
+		if (*array)[i] < pivot {
+			(*array)[i], (*array)[positionalIndex] = (*array)[positionalIndex], (*array)[i]
+			positionalIndex++
+		}
+	}
+	(*array)[end], (*array)[positionalIndex] = (*array)[positionalIndex], (*array)[end]
+
+	return positionalIndex
+}
