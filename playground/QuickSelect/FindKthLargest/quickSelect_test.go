54744ffd57884546ff6273d85ff13379977ef0f8
Cem Gurhan <cemg@live.co.uk>
2023-06-13 07:22:03 +0100

    fix mistake in kth largest

--- playground/QuickSelect/FindKthLargest/quickSelect_test.go
+++ playground/QuickSelect/FindKthLargest/quickSelect_test.go
@@ -10,38 +10,38 @@ func TestFindKthLargest_WithKIsTwo_ReturnsValidNumberFromArray(t *testing.T) {
 	array := []int{3, 6, 1, 10, 8}
 
 	k := 2
-	expectedKthSmallestElement := 8
+	expectedKthLargestElement := 8
 	actualKthSmallestElement := FindKthLargest(&array, 0, len(array)-1, k)
 
-	assert.Equal(t, expectedKthSmallestElement, actualKthSmallestElement)
+	assert.Equal(t, expectedKthLargestElement, actualKthSmallestElement)
 }
 
 func TestFindKthLargest_WithAllPossibleK_ReturnsValidNumbersFromArray(t *testing.T) {
 	array := []int{3, 6, 1, 10, 8}
 
 	k := 1
-	expectedKthSmallestElementOne := 10
+	expectedKthLargestElementOne := 10
 	actualKthSmallestElementOne := FindKthLargest(&array, 0, len(array)-1, k)
 
 	k = 2
-	expectedKthSmallestElement := 8
+	expectedKthLargestElement := 8
 	actualKthSmallestElement := FindKthLargest(&array, 0, len(array)-1, k)
 
 	k = 3
-	expectedKthSmallestElementThree := 6
+	expectedKthLargestElementThree := 6
 	actualKthSmallestElementThree := FindKthLargest(&array, 0, len(array)-1, k)
 
 	k = 4
-	expectedKthSmallestElementFour := 3
+	expectedKthLargestElementFour := 3
 	actualKthSmallestElementFour := FindKthLargest(&array, 0, len(array)-1, k)
 
 	k = 5
-	expectedKthSmallestElementFive := 1
+	expectedKthLargestElementFive := 1
 	actualKthSmallestElementFive := FindKthLargest(&array, 0, len(array)-1, k)
 
-	assert.Equal(t, expectedKthSmallestElementOne, actualKthSmallestElementOne)
-	assert.Equal(t, expectedKthSmallestElement, actualKthSmallestElement)
-	assert.Equal(t, expectedKthSmallestElementThree, actualKthSmallestElementThree)
-	assert.Equal(t, expectedKthSmallestElementFour, actualKthSmallestElementFour)
-	assert.Equal(t, expectedKthSmallestElementFive, actualKthSmallestElementFive)
+	assert.Equal(t, expectedKthLargestElementOne, actualKthSmallestElementOne)
+	assert.Equal(t, expectedKthLargestElement, actualKthSmallestElement)
+	assert.Equal(t, expectedKthLargestElementThree, actualKthSmallestElementThree)
+	assert.Equal(t, expectedKthLargestElementFour, actualKthSmallestElementFour)
+	assert.Equal(t, expectedKthLargestElementFive, actualKthSmallestElementFive)
 }
